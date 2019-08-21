f0ba4e869804ad30b9f95f165f7eaf3e753cc4ac
sahuharikesh <50900606+sahuharikesh@users.noreply.github.com>
2019-08-21 19:01:59 +0530

    Add files via upload
    using nodejs

---
+++ index..js
@@ -0,0 +1,23 @@
+const loadtest = require('loadtest');
+ 
+function statusCallback(error, result, latency) {
+    console.log('Current latency %j, result %j, error %j', latency, result, error);
+    console.log('----');
+    console.log('Request elapsed milliseconds: ', result.requestElapsed);
+    console.log('Request index: ', result.requestIndex);
+    console.log('Request loadtest() instance index: ', result.instanceIndex);
+}
+ 
+const options = {
+    url: 'http://localhost:7357',
+    maxRequests: 1000,
+    statusCallback: statusCallback
+};
+ 
+loadtest.loadTest(options
+    , function(error) {
+    if (error) {
+        return console.error('Got an error: %s', error);
+    }
+    console.log('Tests run successfully');
+});
